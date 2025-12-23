aa5eb9198733e13e54624b48b2fd5c3833b1cb3a
이성준 <leesungjun@iseongjun-ui-MacBookPro.local>
2025-12-23 22:42:49 +0900

    Fix: Add missing useState import in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 
 // ... (SubmitButton code remains same)
 
